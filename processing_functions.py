88b0e9ce8f001ee0ee4ea3e6199d812514f77785
ravioli <ravioli1369@gmail.com>
2023-07-08 05:23:41 +0530

    graphs comparing signal, noise, SNR b/w energybins

--- processing_functions.py
+++ processing_functions.py
@@ -24,6 +24,12 @@ def zero_runs(a):
     max_zeros = np.where(no_of_zeros == np.max(no_of_zeros))[0][0]
     return ranges[max_zeros]
 
+def saa(a):
+    diff = np.diff(a)
+    saa_start = np.argmax(diff)
+    saa_end = saa_start + 1
+    return saa_start, saa_end
+
 def rebin(count_data, time_data, bin_size):
     n = int(bin_size*10)
     rebin_count = [np.median(count_data[i:i+n]) for i in range(0, len(count_data), n)]
@@ -32,53 +38,56 @@ def rebin(count_data, time_data, bin_size):
 
 def filter_and_detrend(filename, start, end, polyorder=3, window=101, data=None):
     if data is None: data = openlc(filename)
-    south_atlantic_start = zero_runs(data['RATE'])[0]
-    south_atlantic_end = zero_runs(data['RATE'])[-1]
-    if end<south_atlantic_start:
-        data['RATE'][start:end] -= (np.mean([data['RATE'][:start]]) + np.mean(data['RATE'][end:south_atlantic_start] + np.mean(data['RATE'][south_atlantic_end:])))/3
+    saa_start = zero_runs(data['RATE'])[0]
+    saa_end = zero_runs(data['RATE'])[-1]
+    if end<saa_start:
+        data['RATE'][start:end] -= (np.mean([data['RATE'][:start]]) + np.mean(data['RATE'][end:saa_start] + np.mean(data['RATE'][saa_end:])))/3
         data['RATE'][:start] -= savgol_filter(data['RATE'][:start], window, polyorder)
-        data['RATE'][end:south_atlantic_start] -= savgol_filter(data['RATE'][end:south_atlantic_start], window, polyorder)
-        data['RATE'][south_atlantic_end:] -= savgol_filter(data['RATE'][south_atlantic_end:], window, polyorder)
-    elif start>south_atlantic_end:
-        data['RATE'][start:end] -= (np.mean([data['RATE'][:south_atlantic_start]]) + np.mean(data['RATE'][south_atlantic_end:start] + np.mean(data['RATE'][end:])))/3
-        data['RATE'][:south_atlantic_start] -= savgol_filter(data['RATE'][:south_atlantic_start], window, polyorder)
-        data['RATE'][south_atlantic_end:start] -= savgol_filter(data['RATE'][south_atlantic_end:start], window, polyorder)
+        data['RATE'][end:saa_start] -= savgol_filter(data['RATE'][end:saa_start], window, polyorder)
+        data['RATE'][saa_end:] -= savgol_filter(data['RATE'][saa_end:], window, polyorder)
+    elif start>saa_end:
+        data['RATE'][start:end] -= (np.mean([data['RATE'][:saa_start]]) + np.mean(data['RATE'][saa_end:start] + np.mean(data['RATE'][end:])))/3
+        data['RATE'][:saa_start] -= savgol_filter(data['RATE'][:saa_start], window, polyorder)
+        data['RATE'][saa_end:start] -= savgol_filter(data['RATE'][saa_end:start], window, polyorder)
         data['RATE'][end:] -= savgol_filter(data['RATE'][end:], window, polyorder)
     else:
         print('Inputted start and end times are not valid')
-    return data, south_atlantic_start, south_atlantic_end
+    return data, saa_start, saa_end
 
 def quadratic(x, a, b, c):
     return a*x**2+b*x+c
 
 def quadratic_detrend(filename, start, end, polyorder=3, window=101, data=None):
     if data is None: data = openlc(filename)
-    south_atlantic_start = zero_runs(data['RATE'])[0]
-    south_atlantic_end = zero_runs(data['RATE'])[-1]
+    saa_start, saa_end = saa(data['TIME'])
     timebin = (data.TIME[end]-data.TIME[start])/(end-start)
     background_window = np.rint(500/timebin).astype(int)
     grb = np.ones_like(data['RATE'][start:end])*np.nan
-    if end<south_atlantic_start:
-        if start>background_window and (south_atlantic_start-end)>background_window:
+    if end<saa_start:
+        if start>background_window and (saa_start-end)>background_window:
             counts = np.concatenate((data['RATE'][start-background_window:start], grb, data['RATE'][end:end+background_window]))
             times = data['TIME'][start-background_window:end+background_window]
-        elif start<background_window and (south_atlantic_start-end)>background_window:
+            new_start, new_end = background_window, background_window+end-start
+        elif start<background_window and (saa_start-end)>background_window:
             counts = np.concatenate((data['RATE'][:start], grb, data['RATE'][end:end+background_window]))
             times = data['TIME'][:end+background_window]
-        elif start>background_window and (south_atlantic_start-end)<background_window:
-            counts = np.concatenate((data['RATE'][start-background_window:start], grb, data['RATE'][end:south_atlantic_start]))
-            times = data['TIME'][start-background_window:south_atlantic_start]
+            new_start, new_end = start, end
+        elif start>background_window and (saa_start-end)<background_window:
+            counts = np.concatenate((data['RATE'][start-background_window:start], grb, data['RATE'][end:saa_start]))
+            times = data['TIME'][start-background_window:saa_start]
+            new_start, new_end = background_window, background_window+end-start
         else:
             counts = np.concatenate((data['RATE'][:start], grb, data['RATE'][end:]))
             times = data['TIME'][:]
-    elif start>south_atlantic_end:
-        if (start-south_atlantic_end)>background_window and (len(data.RATE)-end)>background_window:
+            new_start, new_end = start, end
+    elif start>saa_end:
+        if (start-saa_end)>background_window and (len(data.RATE)-end)>background_window:
             counts = np.concatenate((data['RATE'][start-background_window:start], grb, data['RATE'][end:end+background_window]))
             times = data['TIME'][start-background_window:end+background_window]
-        elif (start-south_atlantic_end)<background_window and (len(data.RATE)-end)>background_window:
-            counts = np.concatenate((data['RATE'][south_atlantic_end:start], grb, data['RATE'][end:end+background_window]))
-            times = data['TIME'][south_atlantic_end:end+background_window]
-        elif (start-south_atlantic_end)>background_window and (len(data.RATE)-end)<background_window:
+        elif (start-saa_end)<background_window and (len(data.RATE)-end)>background_window:
+            counts = np.concatenate((data['RATE'][saa_end:start], grb, data['RATE'][end:end+background_window]))
+            times = data['TIME'][saa_end:end+background_window]
+        elif (start-saa_end)>background_window and (len(data.RATE)-end)<background_window:
             counts = np.concatenate((data['RATE'][start-background_window:start], grb, data['RATE'][end:]))
             times = data['TIME'][start-background_window:]
         else:
@@ -95,16 +104,16 @@ def quadratic_detrend(filename, start, end, polyorder=3, window=101, data=None):
     counts[nans[0]:nans[-1]+1] = data['RATE'][start:end]
     detrended = counts - quadratic(times, *popt)
     t = QTable([times, detrended], names=('TIME', 'RATE'))
-    return t
+    return t, saa_start, saa_end, new_start, new_end
 
 def snr_rms(filename, start, end, polyorder=3):
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder)
-    if end<south_atlantic_start:
+    data, saa_start, saa_end = filter_and_detrend(filename, start, end, polyorder)
+    if end<saa_start:
         rms = np.sqrt(np.mean(data['RATE'][:start]**2))   #ignoring the noise after the GRB
         signal = np.max(data['RATE'][start:end])
         snr = signal/rms
-    elif start>south_atlantic_end:
-        rms = np.sqrt(np.mean(data['RATE'][:south_atlantic_start]**2+data['RATE'][south_atlantic_end:start]**2))
+    elif start>saa_end:
+        rms = np.sqrt(np.mean(data['RATE'][:saa_start]**2+data['RATE'][saa_end:start]**2))
         signal = np.max(data['RATE'][start:end])
         snr = signal/rms
     else:
@@ -113,13 +122,13 @@ def snr_rms(filename, start, end, polyorder=3):
     return snr
 
 def snr_abs(filename, start, end, polyorder=3):
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder)
-    if end<south_atlantic_start:
+    data, saa_start, saa_end = filter_and_detrend(filename, start, end, polyorder)
+    if end<saa_start:
         abs = np.mean(np.abs(data['RATE'][:start]))
         signal = np.max(data['RATE'][start:end])
         snr = signal/abs
-    elif start>south_atlantic_end:
-        abs = (np.mean(np.abs(data['RATE'][:south_atlantic_start]))+np.mean(np.abs(data['RATE'][south_atlantic_end:start])))/2
+    elif start>saa_end:
+        abs = (np.mean(np.abs(data['RATE'][:saa_start]))+np.mean(np.abs(data['RATE'][saa_end:start])))/2
         signal = np.max(data['RATE'][start:end])
         snr = signal/abs
     else:
@@ -131,11 +140,11 @@ def gaussian(x, A, m, s, c):
     return A*np.exp(-(x-m)**2/(2*s**2)) + c
 
 def snr_gauss(filename, start, end, polyorder=3, in_bins=100, window=101, d=None):
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder, window, d)
-    if end<south_atlantic_start:
-        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:south_atlantic_start], data['RATE'][south_atlantic_end:]))
-    elif start>south_atlantic_end:
-        total_noise = np.concatenate((data['RATE'][:south_atlantic_start], data['RATE'][south_atlantic_end:start], data['RATE'][end:]))
+    data, saa_start, saa_end, start, end = quadratic_detrend(filename, start, end, polyorder, window, d)
+    if end<saa_start:
+        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:saa_start], data['RATE'][saa_end:]))
+    elif start>saa_end:
+        total_noise = np.concatenate((data['RATE'][:saa_start], data['RATE'][saa_end:start], data['RATE'][end:]))
     else:
         print('Inputted start and end times are not valid')
     
@@ -153,11 +162,11 @@ def poisson_fit(k, lamb, c):
     return c*poisson.pmf(k, lamb)
 
 def snr_poisson(filename, start, end, polyorder=3, in_bins=100, window=101, d=None):
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder, window, d)
-    if end<south_atlantic_start:
-        noise = np.concatenate((data['RATE'][:start], data['RATE'][end:south_atlantic_start], data['RATE'][south_atlantic_end:]))
-    elif start>south_atlantic_end:
-        noise = np.concatenate((data['RATE'][:south_atlantic_start], data['RATE'][south_atlantic_end:start], data['RATE'][end:]))
+    data, saa_start, saa_end, start, end = quadratic_detrend(filename, start, end, polyorder, window, d)
+    if end<saa_start:
+        noise = np.concatenate((data['RATE'][:start], data['RATE'][end:saa_start], data['RATE'][saa_end:]))
+    elif start>saa_end:
+        noise = np.concatenate((data['RATE'][:saa_start], data['RATE'][saa_end:start], data['RATE'][end:]))
     else:
         print('Inputted start and end times are not valid')
     
@@ -177,11 +186,11 @@ def snr_gamma(filename, start, end, polyorder=3, in_bins=100, window=101, d=None
     def gamma_fit(x, c):
         return c*gamma.pdf(x, k, scale=theta)
     
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder, window, d)
-    if end<south_atlantic_start:
-        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:south_atlantic_start], data['RATE'][south_atlantic_end:]))
-    elif start>south_atlantic_end:
-        total_noise = np.concatenate((data['RATE'][:south_atlantic_start], data['RATE'][south_atlantic_end:start], data['RATE'][end:]))
+    data, saa_start, saa_end, start, end = quadratic_detrend(filename, start, end, polyorder, window, d)
+    if end<saa_start:
+        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:saa_start], data['RATE'][saa_end:]))
+    elif start>saa_end:
+        total_noise = np.concatenate((data['RATE'][:saa_start], data['RATE'][saa_end:start], data['RATE'][end:]))
     else:
         print('Inputted start and end times are not valid')
     
@@ -203,11 +212,11 @@ def snr_skewnorm(filename, start, end, polyorder=3, in_bins=100, window=101, d=N
     def skewnorm_fit(x, k):
         return k*skewnorm.pdf(x, a, scale=scale)
     
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder, window, d)
-    if end<south_atlantic_start:
-        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:south_atlantic_start], data['RATE'][south_atlantic_end:]))
-    elif start>south_atlantic_end:
-        total_noise = np.concatenate((data['RATE'][:south_atlantic_start], data['RATE'][south_atlantic_end:start], data['RATE'][end:]))
+    data, saa_start, saa_end, start, end = quadratic_detrend(filename, start, end, polyorder, window, d)
+    if end<saa_start:
+        total_noise = np.concatenate((data['RATE'][:start], data['RATE'][end:saa_start], data['RATE'][saa_end:]))
+    elif start>saa_end:
+        total_noise = np.concatenate((data['RATE'][:saa_start], data['RATE'][saa_end:start], data['RATE'][end:]))
     else:
         print('Inputted start and end times are not valid')
     
@@ -226,16 +235,16 @@ def snr_skewnorm(filename, start, end, polyorder=3, in_bins=100, window=101, d=N
     return snr, n, bin_center, fit, popt
 
 def snr_counts(filename, start, end, polyorder=3, window=101):
-    data, south_atlantic_start, south_atlantic_end = filter_and_detrend(filename, start, end, polyorder, window)
+    data, saa_start, saa_end = filter_and_detrend(filename, start, end, polyorder, window)
     duration_lc = data['TIME'][-1]-data['TIME'][0]
     duration_burst = data['TIME'][end]-data['TIME'][start]
-    duration_sao = data['TIME'][south_atlantic_end]-data['TIME'][south_atlantic_start]
+    duration_sao = data['TIME'][saa_end]-data['TIME'][saa_start]
     duration_noise = duration_lc-duration_burst-duration_sao
-    if end<south_atlantic_start:
-        noise = (np.sum(np.abs(data['RATE'][:start]))+np.sum(np.abs(data['RATE'][end:south_atlantic_start]))+np.sum(np.abs(data['RATE'][south_atlantic_end:])))/duration_noise
+    if end<saa_start:
+        noise = (np.sum(np.abs(data['RATE'][:start]))+np.sum(np.abs(data['RATE'][end:saa_start]))+np.sum(np.abs(data['RATE'][saa_end:])))/duration_noise
         
-    elif start>south_atlantic_end:
-        noise = (np.sum(np.abs(data['RATE'][:south_atlantic_start]))+np.sum(np.abs(data['RATE'][south_atlantic_end:start]))+np.sum(np.abs(data['RATE'][end:])))/duration_noise
+    elif start>saa_end:
+        noise = (np.sum(np.abs(data['RATE'][:saa_start]))+np.sum(np.abs(data['RATE'][saa_end:start]))+np.sum(np.abs(data['RATE'][end:])))/duration_noise
     else:
         print('Inputted start and end times are not valid')
         snr = 0
